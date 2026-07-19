07238329d180501a17ac86335a5961c4df4ed02d
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-07-19 04:37:24 -0300

    set up mongodb connection with mongoose and dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
+import connectDB from './config/database.js';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+// Connect to MongoDB database
+connectDB();
 
 app.use(express.json());
 
