0b1cc3460fa1436329ccfdefa613f6e3915b931e
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-07-23 00:11:37 -0300

    feat: setup handlebars view engine and main layout

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import connectDB from './config/database.js';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
+import { engine } from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,9 +12,16 @@ const PORT = process.env.PORT || 8080;
 connectDB();
 
 app.use(express.json());
-
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+
+// Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve('src/views'));
+
+// Middleware archivos estáticos
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
